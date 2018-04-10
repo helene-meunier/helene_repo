4f8d4635d6e6da095a4dbaaf486202011256835e
Helene Meunier <helene@Helenes-MBP.local>
2018-04-09 21:38:29 -0400

    Fixed script 1 added script 2

--- script.sh
+++ script.sh
@@ -7,22 +7,36 @@ consonents=("b" "c" "d" "f" "g" "h" "j" "k" "l" "m" "n" "p" "q" "r" "s" "t" "v" 
 vowel=("a" "e" "i" "o" "u")
 
 
-name=();
+declare -a name;
 
 #CVCVC 
 
 name_length=5
 
+function generateRandomName(){
 for i in `seq 1 $name_length`; do
 
 if [[ $(($i % 2)) == 0 ]]; then
-	$name+="${vowel[$(( (RANDOM % 4 ))]}"
 
+name+=("${vowel[$(((RANDOM % 4)+0))]}")
 else
-	$name+="${consonent[$(( (RANDOM % ${#consonents[@]}) )]}"
+
+name+=("${consonents[$(((RANDOM % $((${#consonents[@]}-1)) ) +0))]}")
 fi
-
 done
+for i in ${name[@]}; do
+printf $i
+done
+}
 
-echo $name
 
+
+
+#echo `generateRandomName`
+
+number_of_names=100
+
+for i in `seq 1 $number_of_names`; do
+echo `generateRandomName`
+#echo $i
+done| sort | column
---
+++ script2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+
+
+array=()
+
+array+=("a")
